405e01a4c60924623ffd429b9520ff95cb4949ff
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-09-03 12:49:07 -0500

    Add: calculator pads grid and pad

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { colors } from '../global/colors';
 import { availableFonts } from '../global/typography';
 import cart from '../images/cart.png';
+import backspace from '../images/icons/backspace.svg';
+import checkmark from '../images/icons/checkmark.svg';
+import calendar from '../images/icons/calendar.svg';
 import styled from '@emotion/styled';
 
 function Calculator() {
@@ -17,8 +20,36 @@ function Calculator() {
       <StyledCalculatorDisplay className='calculator__display'>
         <DisplayText>$0</DisplayText>
       </StyledCalculatorDisplay>
-      <StyledCalculatorBody className='calculator__body'>
-        Body
+      <StyledCalculatorBody className='calculator__pads'>
+        {/* First Row */}
+        <StyledPad className='pads__pad'>÷</StyledPad>
+        <StyledPad className='pads__pad'>1</StyledPad>
+        <StyledPad className='pads__pad'>2</StyledPad>
+        <StyledPad className='pads__pad'>3</StyledPad>
+        <StyledPad className='pads__pad'>
+          <img src={backspace} alt='backspace icon' draggable={false} />
+        </StyledPad>
+        {/* Second Row */}
+        <StyledPad className='pads__pad'>x</StyledPad>
+        <StyledPad className='pads__pad'>4</StyledPad>
+        <StyledPad className='pads__pad'>5</StyledPad>
+        <StyledPad className='pads__pad'>6</StyledPad>
+        <StyledPad className='pads__pad'>C</StyledPad>
+        {/* Third Row */}
+        <StyledPad className='pads__pad'>-</StyledPad>
+        <StyledPad className='pads__pad'>7</StyledPad>
+        <StyledPad className='pads__pad'>8</StyledPad>
+        <StyledPad className='pads__pad'>9</StyledPad>
+        <StyledPad className='pads__pad'>
+          <img src={checkmark} alt='checkmark icon' draggable={false} />
+        </StyledPad>
+        {/* Fourth Row */}
+        <StyledPad className='pads__pad'>+</StyledPad>
+        <StyledPad className='pads__pad'>
+          <img src={calendar} alt='calendar icon' draggable={false} />
+        </StyledPad>
+        <StyledPad className='pads__pad'>0</StyledPad>
+        <StyledPad className='pads__pad'>.</StyledPad>
       </StyledCalculatorBody>
       <StyledCalculatorFooter className='calculator__footer'>
         <StyledDate>Thursday March, 10, 2022</StyledDate>
@@ -32,7 +63,9 @@ export default Calculator;
 const StyledCalculator = styled.div`
   font-family: ${availableFonts.primary};
   border: 1px solid #eee;
-  width: 256px;
+  border-radius: 8px;
+  overflow: hidden;
+  width: 254px;
 `;
 
 const StyledCalculatorHeader = styled.div`
@@ -68,7 +101,21 @@ const DisplayText = styled.span`
   color: ${colors.gray[600]};
 `;
 
-const StyledCalculatorBody = styled.div``;
+const StyledCalculatorBody = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+`;
+
+const StyledPad = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  border: 0.5px solid #eee;
+  /* &:last-child {
+    background-color: ${colors.cyan[500]};
+  } */
+`;
 
 const StyledCalculatorFooter = styled.div`
   display: flex;
